0d0d39b3996ae1a9e435de9b02ce6b9f61fd49c1
Piotrek Budzowski <piobudzows@gmail.com>
2025-03-24 19:47:23 +0100

    Update script.js

--- Proyecto 3/script.js
+++ Proyecto 3/script.js
@@ -1,7 +1,17 @@
 function Navigate(index) {
-    let limpieza = document.querySelectorAll('.swiper-slide');
-    limpieza.forEach(slide => {
-        slide.classList.remove('activada'); 
+    let listaSlides = document.querySelectorAll('.swiper-slide');
+    let ahoraActivado = document.querySelector('.swiper-slide.activada');
+    
+    if (ahoraActivado && Array.from(listaSlides).indexOf(ahoraActivado) === index) {
+        return;  // SI ESTA EN MISMA SECCION NO PASA NADA
+    }
+        
+    listaSlides.forEach(slide => {
+        slide.classList.remove('ultima')
+        if (slide.classList.contains('activada')) {
+            slide.classList.remove('activada');
+            slide.classList.add('ultima');
+        }
     });
 
     let eleccionSlides = document.querySelectorAll('.swiper-slide')[index];
